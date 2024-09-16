1181ec7730f694340af4219c13b726ab876c017b
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-16 21:32:21 +0800

    build: create two variables

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,6 @@
 let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
 
 document.querySelector("button").addEventListener("click", () => {
   speech.text = document.querySelector("textarea").value;
